6ae1ac48753cc716fb953a99c139976cbaeb6b54
vedicskill <vedicskill@users.noreply.github.com>
2025-09-14 17:51:40 +0000

    deploy: 98ee346ae8104967b01fead9b2bbcf542a17e09f

--- assets/js/5e95c892.e2d07450.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkvedicskill=self.webpackChunkvedicskill||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});r(6540);var c=r(4164),l=r(7559),u=r(5500),a=r(2831),i=r(9139),d=r(4848);function n(e){return(0,d.jsx)(u.e3,{className:(0,c.A)(l.G.wrapper.docsPages),children:(0,d.jsx)(i.A,{children:(0,a.v)(e.route.routes)})})}}}]);
